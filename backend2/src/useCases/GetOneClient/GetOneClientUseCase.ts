bd7865dda7c459f46aee7f305e2f8e8688a14af8
Lucas Amorim <lucas.ad@hotmail.com>
2022-12-30 15:27:30 -0300

    Criando container Docker para frontend React, Backend Node e MongoDB e useCase GetOneClient

--- backend2/src/useCases/GetOneClient/GetOneClientUseCase.ts
+++ backend2/src/useCases/GetOneClient/GetOneClientUseCase.ts
@@ -4,6 +4,10 @@ import { ClientsRepository } from "../../repositories/mongo/ClientsRepository";
 export class GetOneClientUseCase {
   constructor(private clientsRepository: ClientsRepository) {}
   async execute(id: string): Promise<Client | null> {
+    let clientFounded = await this.clientsRepository.findById(id);
+    if (clientFounded === null) {
+      throw new Error("Client not found");
+    }
     return await this.clientsRepository.findById(id);
   }
 }
