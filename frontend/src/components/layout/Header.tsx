7228a25d4281de6d43523083562d1e1057847f0e
Lucas Amorim <lucas.ad@hotmail.com>
2022-12-30 17:46:29 -0300

    Instalando tailwind e configurando

---
+++ frontend/src/components/layout/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="bg-black">
+      <ul className="bg-black">
+        <li>teste</li>
+        <li>teste</li>
+        <li>teste</li>
+        <li>teste</li>
+      </ul>
+    </header>
+  );
+};
+export { Header };
