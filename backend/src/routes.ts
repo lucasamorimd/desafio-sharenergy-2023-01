341a72d45ce419d9103d7658de286f86db0ff56f
Lucas Amorim <lucas.ad@hotmail.com>
2023-01-06 15:26:35 -0300

    Configurando authorization no service

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -8,13 +8,19 @@ import { updateClientController } from "./factories/UpdateClientFactory";
 
 const router = Router();
 
-router.post("/client/create", (req: Request, res: Response) => {
-  return createClientController.run(req, res);
-});
-
 router.post("/login", (req: Request, res: Response) => {
   return authController.run(req, res);
 });
+
+router.post(
+  "/client/create",
+  (req: Request, res: Response, next: NextFunction) => {
+    return verifyToken.run(req, res, next);
+  },
+  (req: Request, res: Response) => {
+    return createClientController.run(req, res);
+  }
+);
 
 router.get(
   "/client/list",
@@ -26,16 +32,34 @@ router.get(
   }
 );
 
-router.get("/client/:id", (req: Request, res: Response) => {
-  return getOneClientController.run(req, res);
-});
+router.get(
+  "/client/:id",
+  (req: Request, res: Response, next: NextFunction) => {
+    return verifyToken.run(req, res, next);
+  },
+  (req: Request, res: Response) => {
+    return getOneClientController.run(req, res);
+  }
+);
 
-router.put("/client/update", (req: Request, res: Response) => {
-  return updateClientController.run(req, res);
-});
+router.put(
+  "/client/update",
+  (req: Request, res: Response, next: NextFunction) => {
+    verifyToken.run(req, res, next);
+  },
+  (req: Request, res: Response) => {
+    return updateClientController.run(req, res);
+  }
+);
 
-router.delete("/client/delete/:id", (req: Request, res: Response) => {
-  return deleteClientController.run(req, res);
-});
+router.delete(
+  "/client/delete/:id",
+  (req: Request, res: Response, next: NextFunction) => {
+    return verifyToken.run(req, res, next);
+  },
+  (req: Request, res: Response) => {
+    return deleteClientController.run(req, res);
+  }
+);
 
 export { router };
